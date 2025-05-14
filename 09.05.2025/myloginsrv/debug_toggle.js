81c0f2b1af678bc19dd125a444807e4c4bced45c
DerEchteTom <schmidtchen71@hotmail.com>
2025-05-14 15:25:51 +0200

    Aktualisiert: Stand vom 13. Mai 2025 – lokaler Stand ersetzt veraltete Dateien

--- 09.05.2025/myloginsrv/debug_toggle.js
+++
@@ -1,9 +0,0 @@
-// Datei: debug_toggle.js – Version: 2025-05-09_01
-// Beschreibung: JavaScript zur Umschaltung der Debug-Anzeige (Debug-Box)
-
-function toggleDebug() {
-    const box = document.getElementById("debug-box");
-    if (box) {
-        box.style.display = box.style.display === "none" ? "block" : "none";
-    }
-}
